5c7a791e549f3f026a0706b24bf69fe7c527c837
Kyle ROBINSON <w021256j@student.staffs.ac.uk>
2023-02-21 15:46:54 +0000

    Cleaned bloom shaders.

--- FrameworkDX11/Resources/Shaders/Bloom_PS.hlsl
+++ FrameworkDX11/Resources/Shaders/Bloom_PS.hlsl
@@ -1,62 +1,18 @@
+// Resources
+Texture2D texDiffuse : register( t0 );
+SamplerState smpPoint : register( s0 );
 
-
-Texture2D tex : register(t0);
-SamplerState PointSampler : register(s0);
-
-
-cbuffer PostProcessingCB: register(b0)
-{
-    float4  Color;
-    //----------
-    int UseHDR;
-    int UseBloom;
-    int UseDepthOfF;
-    int UseColour;
-    //------------
-    int UseBlur;
-    float fadeAmount;
-    float FarPlane;
-    float focalwidth;
-    //--------------
-    float focalDistance;
-    float blerAttenuation;
-    int pad1;
-    int pad2;
-};
-
-struct QuadVS_Input 
-{
-    float4 Pos : POSITION;
-    float2 Tex : TEXCOORD0;
-};
-
-struct QuadVS_Output 
+// Pixel Shader
+struct VERTEX_OUT 
 {
     float4 Pos : SV_POSITION;
     float2 Tex : TEXCOORD0;
-  
 };
 
-QuadVS_Output QuadVS(QuadVS_Input Input)
+float4 PS( VERTEX_OUT input ) : SV_TARGET
 {
-    // no mvp transform - model coordinates already in projection space (-1 to 1)
-    QuadVS_Output Output;
-    Output.Pos = Input.Pos;
-    Output.Tex = Input.Tex;
-
-  
-    return Output;
-}
-
-//Pixel shader
-float4 QuadPS(QuadVS_Output Input) : SV_TARGET
-{
-    float4 vColor = tex.Sample(PointSampler, Input.Tex);
-    
-    //get what is bight
-    if (vColor.x <= 1 || vColor.y <= 1|| vColor.z <= 1|| vColor.a <= 1) {
+    float4 vColor = texDiffuse.Sample( smpPoint, input.Tex );
+    if ( vColor.x <= 1 || vColor.y <= 1|| vColor.z <= 1|| vColor.a <= 1 )
         vColor = 0;
-    }
-    
     return vColor;
 }
--- FrameworkDX11/Resources/Shaders/Bloom_VS.hlsl
+++ FrameworkDX11/Resources/Shaders/Bloom_VS.hlsl
@@ -1,62 +1,20 @@
-
-
-Texture2D tex : register(t0);
-SamplerState PointSampler : register(s0);
-
-
-cbuffer PostProcessingCB: register(b0)
-{
-    float4  Color;
-    //----------
-    int UseHDR;
-    int UseBloom;
-    int UseDepthOfF;
-    int UseColour;
-    //------------
-    int UseBlur;
-    float fadeAmount;
-    float FarPlane;
-    float focalwidth;
-    //--------------
-    float focalDistance;
-    float blerAttenuation;
-    int pad1;
-    int pad2;
-};
-
-struct QuadVS_Input 
+// Vertex Shader
+struct VERTEX_IN
 {
     float4 Pos : POSITION;
     float2 Tex : TEXCOORD0;
 };
 
-struct QuadVS_Output 
+struct VERTEX_OUT 
 {
     float4 Pos : SV_POSITION;
     float2 Tex : TEXCOORD0;
-  
 };
 
-QuadVS_Output QuadVS(QuadVS_Input Input)
+VERTEX_OUT VS( VERTEX_IN input )
 {
-    // no mvp transform - model coordinates already in projection space (-1 to 1)
-    QuadVS_Output Output;
-    Output.Pos = Input.Pos;
-    Output.Tex = Input.Tex;
-
-  
+    VERTEX_OUT output;
+    output.Pos = input.Pos;
+    output.Tex = input.Tex;
     return Output;
-}
-
-//Pixel shader
-float4 QuadPS(QuadVS_Output Input) : SV_TARGET
-{
-    float4 vColor = tex.Sample(PointSampler, Input.Tex);
-    
-    //get what is bight
-    if (vColor.x <= 1 || vColor.y <= 1|| vColor.z <= 1|| vColor.a <= 1) {
-        vColor = 0;
-    }
-    
-    return vColor;
 }
