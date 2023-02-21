4d64d5dbeea3e532d2be166b57d3e65051b75287
Kyle ROBINSON <w021256j@student.staffs.ac.uk>
2023-02-21 16:33:01 +0000

    Cleaned solid colour shaders.

--- FrameworkDX11/Resources/Shaders/SolidColour_PS.hlsl
+++ FrameworkDX11/Resources/Shaders/SolidColour_PS.hlsl
@@ -1,50 +1,17 @@
+// Resources
+Texture2D texDiffuse : register( t0 );
+SamplerState smpPoint : register( s0 );
 
-
-Texture2D tex : register(t0);
-SamplerState PointSampler : register(s0);
-
-
-cbuffer PostProcessingCB: register(b0)
-{
-    float4  Color;
-    //----------
-    int UseHDR;
-    int UseBloom;
-    int UseDepthOfF;
-    int UseColour;
-    //------------
-    int UseBlur;
-    float fadeAmount;
-    int pad2;
-    int pad3;
-};
-
-struct QuadVS_Input
-{
-    float4 Pos : POSITION;
-    float2 Tex : TEXCOORD0;
-};
-
-struct QuadVS_Output
+// Pixel Shader
+struct VERTEX_OUT
 {
     float4 Pos : SV_POSITION;
     float2 Tex : TEXCOORD0;
 };
 
-QuadVS_Output QuadVS(QuadVS_Input Input)
-{
-    // no mvp transform - model coordinates already in projection space (-1 to 1)
-    QuadVS_Output Output;
-    Output.Pos = Input.Pos;
-    Output.Tex = Input.Tex;
-
-    return Output;
-}
-
 //Pixel shader
-float4 QuadPS(QuadVS_Output Input) : SV_TARGET
+float4 PS( VERTEX_OUT input ) : SV_TARGET
 {
-    float4 vColor = tex.Sample(PointSampler, Input.Tex);
-
+    float4 vColor = texDiffuse.Sample( smpPoint, input.Tex );
     return vColor;
 }
--- FrameworkDX11/Resources/Shaders/SolidColour_VS.hlsl
+++ FrameworkDX11/Resources/Shaders/SolidColour_VS.hlsl
@@ -1,50 +1,20 @@
-
-
-Texture2D tex : register(t0);
-SamplerState PointSampler : register(s0);
-
-
-cbuffer PostProcessingCB: register(b0)
-{
-    float4  Color;
-    //----------
-    int UseHDR;
-    int UseBloom;
-    int UseDepthOfF;
-    int UseColour;
-    //------------
-    int UseBlur;
-    float fadeAmount;
-    int pad2;
-    int pad3;
-};
-
-struct QuadVS_Input
+// Vertex Shader
+struct VERTEX_IN
 {
     float4 Pos : POSITION;
     float2 Tex : TEXCOORD0;
 };
 
-struct QuadVS_Output
+struct VERTEX_OUT
 {
     float4 Pos : SV_POSITION;
     float2 Tex : TEXCOORD0;
 };
 
-QuadVS_Output QuadVS(QuadVS_Input Input)
+VERTEX_OUT VS( VERTEX_IN input )
 {
-    // no mvp transform - model coordinates already in projection space (-1 to 1)
-    QuadVS_Output Output;
-    Output.Pos = Input.Pos;
-    Output.Tex = Input.Tex;
-
-    return Output;
-}
-
-//Pixel shader
-float4 QuadPS(QuadVS_Output Input) : SV_TARGET
-{
-    float4 vColor = tex.Sample(PointSampler, Input.Tex);
-
-    return vColor;
+    VERTEX_OUT output;
+    output.Pos = input.Pos;
+    output.Tex = input.Tex;
+    return output;
 }
