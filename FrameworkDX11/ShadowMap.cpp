038f5c800be51a6fd4b951ac61771b628c48a2e3
Kyle Robinson <w02126j@student.staffs.ac.uk>
2023-02-24 02:05:36 +0000

    Cleaned shadow map class.

--- FrameworkDX11/ShadowMap.cpp
+++ FrameworkDX11/ShadowMap.cpp
@@ -1,12 +1,13 @@
+#include "stdafx.h"
 #include "ShadowMap.h"
-ShadowMap::ShadowMap(ID3D11Device* device, UINT width, UINT height)
-	: mWidth(width), mHeight(height), mDepthMapSRV(nullptr), mDepthMapDSV(nullptr)
+
+ShadowMap::ShadowMap( ID3D11Device* device, UINT width, UINT height ) :
+	m_uWidth( width ), m_uHeight( height ), m_pDepthMapSRV( nullptr ), m_pDepthMapDSV( nullptr )
 {
 
-	ID3D11Texture2D* depthMap = nullptr;
 	D3D11_TEXTURE2D_DESC texDesc;
-	texDesc.Width = mWidth;
-	texDesc.Height = mHeight;
+	texDesc.Width = m_uWidth;
+	texDesc.Height = m_uHeight;
 	texDesc.MipLevels = 1;
 	texDesc.ArraySize = 1;
 	texDesc.Format = DXGI_FORMAT_R32_TYPELESS;
@@ -16,57 +17,58 @@ ShadowMap::ShadowMap(ID3D11Device* device, UINT width, UINT height)
 	texDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL | D3D11_BIND_SHADER_RESOURCE;
 	texDesc.CPUAccessFlags = 0;
 	texDesc.MiscFlags = 0;
-
-	HRESULT HR =device->CreateTexture2D(&texDesc, 0, &depthMap);
+	ID3D11Texture2D* depthMap = nullptr;
+	HRESULT hr = device->CreateTexture2D( &texDesc, 0, &depthMap );
+	if ( FAILED( hr ) )
+		return;
 
 	D3D11_DEPTH_STENCIL_VIEW_DESC dsvDesc;
 	dsvDesc.Flags = 0;
 	dsvDesc.Format = DXGI_FORMAT_D32_FLOAT;
 	dsvDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
 	dsvDesc.Texture2D.MipSlice = 0;
-	HR = device->CreateDepthStencilView(depthMap, &dsvDesc, &mDepthMapDSV);
+	hr = device->CreateDepthStencilView( depthMap, &dsvDesc, &m_pDepthMapDSV );
+	if ( FAILED( hr ) )
+		return;
 
 	D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
 	srvDesc.Format = DXGI_FORMAT_R32_FLOAT;
 	srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 	srvDesc.Texture2D.MipLevels = 1;
 	srvDesc.Texture2D.MostDetailedMip = 0;
-	HR = device->CreateShaderResourceView(depthMap, &srvDesc, &mDepthMapSRV);
-	
+	hr = device->CreateShaderResourceView( depthMap, &srvDesc, &m_pDepthMapSRV );
+	if ( FAILED( hr ) )
+		return;
 
 	depthMap->Release();
-	
 }
 
 ShadowMap::~ShadowMap()
 {
-
 	CleanUp();
 }
 
-
 ID3D11ShaderResourceView* ShadowMap::DepthMapSRV()
 {
-	return mDepthMapSRV;
+	return m_pDepthMapSRV;
 }
 
-void ShadowMap::SetShadowMap(ID3D11DeviceContext* dc)
+void ShadowMap::SetShadowMap( ID3D11DeviceContext* pContext )
 {
 	ID3D11RenderTargetView* renderTargets[1] = { 0 };
-	dc->OMSetRenderTargets(1, renderTargets, mDepthMapDSV);
-	
-	dc->ClearDepthStencilView(mDepthMapDSV, D3D11_CLEAR_DEPTH, 1.0f, 0);
+	pContext->OMSetRenderTargets( 1, renderTargets, m_pDepthMapDSV );
+	pContext->ClearDepthStencilView( m_pDepthMapDSV, D3D11_CLEAR_DEPTH, 1.0f, 0 );
 }
 
 void ShadowMap::CleanUp()
 {
-	if (mDepthMapSRV) {
-		mDepthMapSRV->Release();
+	if ( m_pDepthMapSRV )
+	{
+		m_pDepthMapSRV->Release();
 	}
-	if (mDepthMapDSV) {
-		mDepthMapDSV->Release();
+
+	if ( m_pDepthMapDSV )
+	{
+		m_pDepthMapDSV->Release();
 	}
 }
-
-
-
--- FrameworkDX11/ShadowMap.h
+++ FrameworkDX11/ShadowMap.h
@@ -1,34 +1,30 @@
 #pragma once
-#include <d3d11_1.h>
-#include <d3dcompiler.h>
-#include <directxmath.h>
+#ifndef SHADOWMAP_H
+#define SHADOWMAP_H
 
-//shadows implmented with the help of 
-//https://www.youtube.com/watch?v=CIGNP71FiG0
-//https://www.youtube.com/watch?v=EPHjOUQ4rC8&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=64
-//https://www.youtube.com/watch?v=tRsJ-b2ngIQ&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=65
+// REFERENCES
+// https://www.youtube.com/watch?v=CIGNP71FiG0
+// https://www.youtube.com/watch?v=EPHjOUQ4rC8&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=64
+// https://www.youtube.com/watch?v=tRsJ-b2ngIQ&list=PLqCJpWy5Fohd3S7ICFXwUomYW0Wv67pDD&index=65
 
-using namespace DirectX;
-/// <summary>
-/// stores the shadow map data
-/// </summary>
 class ShadowMap
 {
-
 public:
-	ShadowMap(ID3D11Device* device, UINT width, UINT height);
+	ShadowMap( ID3D11Device* device, UINT width, UINT height );
 	~ShadowMap();
+
 	ID3D11ShaderResourceView* DepthMapSRV();
-	void SetShadowMap(ID3D11DeviceContext* dc);
+	void SetShadowMap( ID3D11DeviceContext* pContext );
 
 private:
-	UINT mWidth;
-	UINT mHeight;
-	ID3D11ShaderResourceView* mDepthMapSRV;
-	ID3D11DepthStencilView* mDepthMapDSV;
-	D3D11_VIEWPORT mViewport;
-	
 	void CleanUp();
 
+	UINT m_uWidth;
+	UINT m_uHeight;
+
+	D3D11_VIEWPORT m_viewport;
+	ID3D11DepthStencilView* m_pDepthMapDSV;
+	ID3D11ShaderResourceView* m_pDepthMapSRV;
 };
 
+#endif
